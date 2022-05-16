3ae485e1cf15fb08833891e574dc2d1f9d3d8446
junghyunhwang <dmagk560@gmail.com>
2022-05-16 22:08:25 +0900

    Add user class

---
+++ Assignment1/src/academy/pocu/comp2500/assignment1/User.java
@@ -0,0 +1,27 @@
+package academy.pocu.comp2500.assignment1;
+
+/*
+    내가 글을 쓴다는 개념이면 Post 생성자 매개변수도 바꿔야 함
+    public Post writePost(Sting title, String body) {
+        return new Post(this.fullName, title, body);
+    }
+
+    User 타입이 필요한지?
+*/
+public class User {
+    private final UserType type;
+    private final String name;
+
+    public User(String name, UserType type) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return this.name;
+    }
+
+    public UserType getUserType() {
+        return this.type;
+    }
+}
