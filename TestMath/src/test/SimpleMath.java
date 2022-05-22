d966d732ac5f45c1a2e571173b0ac29cb4895030
junghyunhwang <dmagk560@gmail.com>
2022-05-22 12:45:52 +0900

    Add test

---
+++ TestMath/src/test/SimpleMath.java
@@ -0,0 +1,19 @@
+package test;
+
+public class SimpleMath {
+    public static int abs(int a) {
+        return a < 0 ? -a : a;
+    }
+
+    public static int abs(int a, int b) {
+        return a + b;
+    }
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+}
---
+++ TestMath/src/test/app/Program.java
@@ -0,0 +1,9 @@
+package test.app;
+
+import test.SimpleMath;
+
+public class Program {
+    private static void main(String[] args) {
+        System.out.println(SimpleMath.abs(8));
+    }
+}
