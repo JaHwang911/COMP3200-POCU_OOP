a027586bfb2236c44d3bba9809b36f0f9f89777e
junghyunhwang <dmagk560@gmail.com>
2022-06-08 16:45:35 +0900

    Add this keyword

--- Lab6/src/academy/pocu/comp2500/lab6/Course.java
+++ Lab6/src/academy/pocu/comp2500/lab6/Course.java
@@ -22,9 +22,9 @@ public class Course extends Menu {
     }
 
     public boolean isValid() {
-        return this.appetizers.size() == maxAppetizerCount
-                && this.mainCourses.size() == maxMainCourseCount
-                && this.desserts.size() == maxDessertCount;
+        return this.appetizers.size() == this.maxAppetizerCount
+                && this.mainCourses.size() == this.maxMainCourseCount
+                && this.desserts.size() == this.maxDessertCount;
     }
 
     public ArrayList<Appetizer> getAppetizers() {
