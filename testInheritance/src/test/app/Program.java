24a26671ccfbefdad6813dec23c742e353d5dc41
junghyunhwang <dmagk560@gmail.com>
2022-06-11 20:31:50 +0900

    Add casting test

--- testInheritance/src/test/app/Program.java
+++ testInheritance/src/test/app/Program.java
@@ -4,18 +4,8 @@ import test.*;
 
 public class Program {
     public static void main(String[] airgs) {
-        Student Ja = new Student("Ja", "Hwang", Houses.HUFFLEPUFF);
-
-        Student harry = new Student("Harry", "Potter", Houses.GRYFFINDOR);
-        Student lon = new Student("Ron", "Weasley", Houses.GRYFFINDOR);
-        Student hermione = new Student("Hermione", "Granger", Houses.GRYFFINDOR);
-
-        Teacher hagird = new Teacher("Rubeus", "Hagrid", Department.MATERIAL);
-
-        Person corruption = Ja;
-        Student promotion = (Student) corruption;
-
-        System.out.println(promotion.getHouse());
+        Person person = new Person("tes", "t");
+        Teacher teacher = (Teacher) person;
 
         System.out.println("No prob");
     }
