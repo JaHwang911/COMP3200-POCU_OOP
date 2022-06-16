dc2742c7082747a1a5e368aca961381744655961
junghyunhwang <dmagk560@gmail.com>
2022-06-16 09:17:23 +0900

    Add test singleton

---
+++ TestSingleton1/src/Test/Configuration.java
@@ -0,0 +1,48 @@
+package Test;
+
+public class Configuration {
+    private static Configuration instance;
+    private int x;
+    private int y;
+    private final int width;
+    private final int height;
+
+    private Configuration() {
+        this.x = 50;
+        this.y = 50;
+        this.width = 600;
+        this.height = 400;
+    }
+
+    public static Configuration getInstance() {
+        if (instance == null) {
+            instance = new Configuration();
+        }
+
+        return instance;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setX(int position) {
+        this.x = position;
+    }
+
+    public void setY(int position) {
+        this.y = position;
+    }
+}
---
+++ TestSingleton1/src/Test/app/Program.java
@@ -0,0 +1,28 @@
+package Test.app;
+
+import Test.Configuration;
+
+public class Program {
+    public static void main(String[] args) {
+        Configuration config0 = Configuration.getInstance();
+
+        assert config0.getX() == 50;
+        assert config0.getY() == 50;
+        assert config0.getWidth() == 600;
+        assert config0.getHeight() == 400;
+
+        config0.setX(100);
+        config0.setY(100);
+        
+        Configuration config1 = Configuration.getInstance();
+
+        assert config1.getX() == 100;
+        assert config1.getY() == 100;
+        assert config1.getWidth() == 600;
+        assert config1.getHeight() == 400;
+
+        assert config0 == config1;
+
+        System.out.println("Singleton test No prob");
+    }
+}
