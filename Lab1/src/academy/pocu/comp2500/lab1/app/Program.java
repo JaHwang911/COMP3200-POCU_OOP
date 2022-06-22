38febd3705390adb441a1aceb3a5af2a5c892982
junghyunhwang <dmagk560@gmail.com>
2022-06-22 16:40:41 +0900

    Fix lab1 for javac compile

--- Lab1/src/academy/pocu/comp2500/lab1/app/Program.java
+++ Lab1/src/academy/pocu/comp2500/lab1/app/Program.java
@@ -3,8 +3,8 @@ package academy.pocu.comp2500.lab1.app;
 import academy.pocu.comp2500.lab1.Lab1;
 
 public class Program {
-
     public static void main(String[] args) {
-        Lab1.test_print_string();
+        System.out.printf("%d + %d = %d%s", 64, 32, Lab1.add(64, 32), System.lineSeparator());
+        System.out.println("No prob");
     }
 }
