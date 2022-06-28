088f26651ac7c5e84555ce63d68e99c85bfae5d4
junghyunhwang <dmagk560@gmail.com>
2022-06-28 23:07:51 +0900

    Add official test

--- Lab7/src/academy/pocu/comp2500/lab7/app/Program.java
+++ Lab7/src/academy/pocu/comp2500/lab7/app/Program.java
@@ -11,8 +11,45 @@ public class Program {
         testCompareBook();
         testBundle();
         testReadingList();
+        testOfficial();
 
         System.out.println("No prob: lab7");
+    }
+
+    public static void testOfficial() {
+        Author author = new Author("James", "Bond");
+        Book book0 = new Book("How to be the best", author, 1990, Genre.BIOGRAPHY);
+        Bookshelf bookshelf = new Bookshelf(10);
+
+        assert (bookshelf.add(book0));
+        assert (bookshelf.remove(book0));
+        assert (!bookshelf.remove(book0));
+
+        Book book1 = new Book("C# for dummies", new Author("Jason", "Bourne"), 2005, Genre.ROMANCE);
+        Book book2 = new Book("C# for dummies", new Author("Jason", "Bourne"), 2005, Genre.ROMANCE);
+        Book book3 = new Book("Java for dummies", new Author("James", "Bond"), 2007, Genre.MYSTERY);
+
+        Bundle bundle = new Bundle("Programming");
+
+        assert (bundle.add(book0));
+        assert (bundle.add(book1));
+        assert (!bundle.add(book2));
+        assert (bundle.add(book3));
+
+        assert (bundle.remove(book3));
+        assert (bundle.remove(book0));
+        assert (!bundle.remove(book0));
+
+        ReadingList readingList = new ReadingList("Summer Break Homework");
+
+        readingList.add(book0);
+        readingList.add(book1);
+        readingList.add(book2);
+        readingList.add(book3);
+
+        assert (readingList.remove(book3));
+        assert (readingList.remove(book0));
+        assert (!readingList.remove(book0));
     }
 
     private static void testCompareAuthor() {
