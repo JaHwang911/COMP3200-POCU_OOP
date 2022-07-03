3264c143f2c85633a9a0df7fc98ff83c5d433e61
junghyunhwang <dmagk560@gmail.com>
2022-07-04 03:05:48 +0900

    Add limit bundle count

--- Lab7/src/academy/pocu/comp2500/lab7/Bundle.java
+++ Lab7/src/academy/pocu/comp2500/lab7/Bundle.java
@@ -3,6 +3,7 @@ package academy.pocu.comp2500.lab7;
 import java.util.HashSet;
 
 public class Bundle {
+    private static final int MAX_BUNDLE_COUNT = 4;
     private final String name;
     private final HashSet<Book> books = new HashSet<>();
 
@@ -15,6 +16,10 @@ public class Bundle {
     }
 
     public boolean add(Book book) {
+        if (books.size() >= MAX_BUNDLE_COUNT) {
+            return false;
+        }
+
         return this.books.add(book);
     }
 
