4d4bce2d972068f935d27c71b9f5ca0a2f3516ae
junghyunhwang <dmagk560@gmail.com>
2022-07-08 20:12:12 +0900

    Add Drainer

--- Lab8/src/academy/pocu/comp2500/lab8/Drainer.java
+++ Lab8/src/academy/pocu/comp2500/lab8/Drainer.java
@@ -1,17 +1,37 @@
 package academy.pocu.comp2500.lab8;
 
-public class Drainer extends SmartDevice implements IDetectable{
-    private int maxWaterAmount;
+public class Drainer extends SmartDevice implements IDetectable {
+    private static final int DRAINING_PER_TICK = 7;
+
+    private final int maxWaterAmount;
+    private Planter planter;
 
     public Drainer(int maxWaterAmount) {
         this.maxWaterAmount = maxWaterAmount;
     }
 
     public void onTick() {
-
+        ++super.tickCount;
     }
 
-    public void detected(Planter planter) {
+    public void installedPlanter(Planter planter) {
+        this.planter = planter;
+        planter.installDetectable(this);
+    }
 
+    public void detected() {
+        onTick();
+
+        if (this.planter != null && this.planter.getWaterAmount() >= maxWaterAmount) {
+            if (!super.isOn) {
+                super.isOn = true;
+                super.tickLastUpdate = super.tickCount;
+            }
+
+            this.planter.subWater(DRAINING_PER_TICK);
+        } else if (super.isOn) {
+            super.isOn = false;
+            super.tickLastUpdate = super.tickCount;
+        }
     }
 }
