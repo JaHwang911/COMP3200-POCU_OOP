f6a37b65d07b75943eb1de56e7c5d5abc183a57f
junghyunhwang <dmagk560@gmail.com>
2022-07-25 17:27:00 +0900

    Add registry

--- Assignment4/src/academy/pocu/comp2500/assignment4/App.java
+++ Assignment4/src/academy/pocu/comp2500/assignment4/App.java
@@ -4,9 +4,13 @@ import academy.pocu.comp2500.assignment4.registry.Registry;
 
 public class App {
     public App(Registry registry) {
-        // Register like this
-        // registry.registerDrawPixelCommandCreator("Foo");
-        // OR
-        // registry.registerDrawPixelCommandCreator("Foo", "bar");
+        registry.registerDrawPixelCommandCreator("DrawPixelCommand");
+        registry.registerIncreasePixelCommandCreator("IncreasePixelCommand");
+        registry.registerDecreasePixelCommandCreator("DecreasePixelCommand");
+        registry.registerFillHorizontalLineCommandCreator("FillHorizontalLineCommand");
+        registry.registerFillVerticalLineCommandCreator("FillVerticalLineCommand");
+        registry.registerToUppercaseCommandCreator("ToUpperCommand");
+        registry.registerToLowercaseCommandCreator("ToLowerCommand");
+        registry.registerClearCommandCreator("ClearCommand");
     }
 }
