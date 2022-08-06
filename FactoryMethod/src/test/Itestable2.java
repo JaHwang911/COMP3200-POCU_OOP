1e2098093a9d660020641cefe1c6da7707bb42db
Ja_Hwang <dmagk560@gmail.com>
2022-08-06 13:20:03 +0900

    Add test

---
+++ FactoryMethod/src/test/Itestable2.java
@@ -0,0 +1,5 @@
+package test;
+
+public interface Itestable2 {
+    int add(int a, int b);
+}
---
+++ FactoryMethod/src/test/Menu.java
@@ -0,0 +1,6 @@
+package test;
+
+public abstract class Menu {
+
+    public abstract Cup createCupOrNull(CupSize size);
+}
